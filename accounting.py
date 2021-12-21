60977c7bc5da37a1e462199cfc2cfc8ebe9bb0a4
Rae Carter <92331343+heimdalalr@users.noreply.github.com>
2021-12-21 12:53:17 -0500

    working through functions

--- accounting.py
+++ accounting.py
@@ -1,26 +1,30 @@
-SALESPERSON_INDEX = 0
-INTERNET_INDEX = 1
-DORKY_LINE_LENGTH = 80
+# SALESPERSON_INDEX = 0
+# INTERNET_INDEX = 1
+# DORKY_LINE_LENGTH = 80
 
-print("*" * DORKY_LINE_LENGTH)
-f = open("orders-by-type.txt")
-melon_tallies = {"Musk":0, "Hybrid":0, "Watermelon":0, "Winter": 0}
+def dorky_line(): # declares the fuctions
+    print("*" * 80) #prints a line of 80 asterisks
 
-for l in f:
-    data = l.split("|")
+dorky_line()#calls the dorky_line function
+
+orders = open("orders-by-type.txt") #opens the file specified
+melon_tally = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0} #creates a dictionary of melon keys with initial values set to 0
+
+for line in orders: #iterates through each line in an open file
+    data = line.split("|")
     melon_type = data[1]
     melon_count = int(data[2])
-    melon_tallies[melon_type] += melon_count
+    melon_tally[melon_type] += melon_count
 
 f.close()
 melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
 total_revenue = 0
-for melon_type in melon_tallies:
+for melon_type in melon_tally:
     price = melon_prices[melon_type]
-    revenue = price * melon_tallies[melon_type]
+    revenue = price * melon_tally[melon_type]
     total_revenue += revenue
     # print("We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue))
-    print(f"We sold {melon_tallies[melon_type]} {melon_type} melons at {price:.2f} each for a total of {revenue:.2f}")
+    print(f"We sold {melon_tally[melon_type]} {melon_type} melons at {price:.2f} each for a total of {revenue:.2f}")
 print("******************************************")
 f = open("orders-with-sales.txt")
 sales = [0, 0]
